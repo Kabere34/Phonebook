f130dd2989eefe2777e8f35f69647a93ff41366c
Ivy Kabere <kabereivy@gmail.com>
2022-10-05 10:42:29 +0300

    update search function

--- src/search/ContactSearch.js
+++ src/search/ContactSearch.js
@@ -1,52 +1,76 @@
-// import React,{useState, useEffect} from 'react'
-// // import { useDispatch } from "react-redux";
-// import axios from "axios";
+import React, { useState, useEffect } from "react";
+ const searchterm ={
+    term:''
+ }
+function SearchComponent() {
 
-// const searchContactUrl =' http://127.0.0.1:8000/phonebook/search/?search={}'
-// const searchcontactPost = ()=> axios.post(searchContactUrl)
+  const [searchterm, setSearch] = React.useState(null);
+  const [receivedData, setReceivedData] = React.useState('');
 
-
-// // const contactSearch ={
-// // firstName :'',
-// // lastName : '',
-// // phoneNumber : '',
-// // email : ''
-// // }
-
-// function SearchContact() {
-
-//     const [search, setSearch] = React.useState('')
-
-//     // const dispatch = useDispatch()
-
-//     const handleSubmit = (e) => {
-//         e.preventDefault();
-//         searchcontactPost(newContact)
-//         console.log("dispatch",newContact)
-//     }
+  const searchUrl =
+    `http://127.0.0.1:8000/phonebook/search/?search=${searchterm}`;
 
 
+    React.useEffect(() => {
+        setContacts();
+    }, []);
 
+    // const url = `http://127.0.0.1:8000/phonebook/searching/?search=${searchterm.searchterm}`
 
-//   return (
-//     <div>
-//         <form onSubmit={handleSubmit}>
-//         <fieldset>
+    let setContacts = async () => {
+        let response = await fetch(`http://127.0.0.1:8000/phonebook/search/?search=${searchterm.searchterm}`);
+        let data = await response.json();
+        setReceivedData(data)
+        console.log(data,'data')
+    }
+     console.log("received from search data: ",receivedData )
 
-//         <label>First Name</label>
-//         <input type='text' name='firstName' onChange={(e) =>
-//               setNewContact({
-//                 ...newContact,
-//                 [e.target.name]: e.target.value,
-//               })
-//             } ></input>
-//         </fieldset>
-//         <button type='submit'>search</button>
+     const handleSubmit = (e) => {
+            e.preventDefault();
+            setContacts()
+            console.log("searching for: ",searchterm.searchterm )
+        }
 
+  return (
+    <div>
+        <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label>search</label>
+          <input
+            type="text"
+            name="searchterm"
+            onChange={(e) =>
+              setSearch({
 
-//       </form>
-//     </div>
-//   )
-// }
+                searchterm: e.target.value,
+              })
+            }
+          ></input>
+        </fieldset>
+        <button type="submit">submit</button>
+      </form>
 
-// export default SearchContact;
+      {receivedData ? (
+
+        <div className="container">
+
+        {receivedData.map((contact, index) =>(
+
+          <div className="row">
+            <div className="col-sm-3">{contact.id}   ---{contact.firstName}</div>
+            <div className="col-sm-3">{contact.lastName}</div>
+            <div className="col-sm-3">{contact.phoneNumber}</div>
+            <div className="col-sm-3">{contact.email}</div>
+          </div>
+        ))}
+        </div>
+
+      ):(
+        <div></div>
+      )
+    }
+    </div>
+  )
+}
+
+export default SearchComponent
