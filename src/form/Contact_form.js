accdbc9858109b54155f24030ef06717c9d1d998
Ivy Kabere <kabereivy@gmail.com>
2022-10-06 11:08:08 +0300

    update contact form

--- src/form/Contact_form.js
+++ src/form/Contact_form.js
@@ -1,86 +1,109 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 // import { useDispatch } from "react-redux";
 import axios from "axios";
-import "./contact_form.css"
+import { Link } from "react-router-dom";
 
-const newContactUrl ='http://127.0.0.1:8000/phonebook/contactAdd'
-const newcontactPost = (contact)=> axios.post(newContactUrl,contact)
+import "./contact_form.css";
 
+const newContactUrl = "http://127.0.0.1:8000/phonebook/contactAdd";
+const newcontactPost = (contact) => axios.post(newContactUrl, contact);
 
-const initialstate ={
-firstName :'',
-lastName : '',
-phoneNumber : '',
-email : ''
-}
+const initialstate = {
+  firstName: "",
+  lastName: "",
+  phoneNumber: "",
+  email: "",
+};
 
 function CreateContact() {
+  const [newContact, setNewContact] = React.useState(initialstate);
 
-    const [newContact, setNewContact] = React.useState(initialstate)
+  // const dispatch = useDispatch()
 
-    // const dispatch = useDispatch()
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        newcontactPost(newContact)
-        console.log("dispatch",newContact)
-    }
-
-
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    newcontactPost(newContact);
+    console.log("dispatch", newContact);
+  };
 
   return (
-    <div className='contact_form'>
-        <form onSubmit={handleSubmit}>
-        <fieldset>
-          <div className='form_title'>
-            <div className='contact_title'><h2>Add Contact</h2></div></div>
+    <>
+      {" "}
+      <div className='menu-barContainer'>
+        <div class='menu-bar'>
+          {/* links to pages */}
+          <Link to='/'>
+            <span>Home</span>
+          </Link>
 
 
-
-
-        <label>First Name</label>
-        <input type='text' name='firstName' onChange={(e) =>
-              setNewContact({
-                ...newContact,
-                [e.target.name]: e.target.value,
-              })
-            } ></input>
-
-        <label>Last Name</label>
-        <input type='text' name='lastName' onChange={(e) =>
-              setNewContact({
-                ...newContact,
-                [e.target.name]: e.target.value,
-              })
-            } ></input>
-
-        <label>Phone number</label>
-        <input type='tel' name='phoneNumber' onChange={(e) =>
-              setNewContact({
-                ...newContact,
-                [e.target.name]: e.target.value,
-              })
-            } ></input>
-
-        <label>Email</label>
-        <input type='email' name='email' onChange={(e) =>
-              setNewContact({
-                ...newContact,
-                [e.target.name]: e.target.value,
-              })
-            } ></input>
-
-        </fieldset>
-        <div className='button_container'>
-        <button type='submit'>submit</button>
+          <span></span>
         </div>
+      </div>
+      <div className='contact_form'>
+        <form onSubmit={handleSubmit}>
+          <fieldset>
+            <div className='form_title'>
+              <div className='contact_title'>
+                <h2>Add Contact</h2>
+              </div>
+            </div>
 
+            <label>First Name</label>
+            <input
+              type='text'
+              name='firstName'
+              onChange={(e) =>
+                setNewContact({
+                  ...newContact,
+                  [e.target.name]: e.target.value,
+                })
+              }
+            ></input>
 
+            <label>Last Name</label>
+            <input
+              type='text'
+              name='lastName'
+              onChange={(e) =>
+                setNewContact({
+                  ...newContact,
+                  [e.target.name]: e.target.value,
+                })
+              }
+            ></input>
 
-      </form>
-    </div>
-  )
+            <label>Phone number</label>
+            <input
+              type='tel'
+              name='phoneNumber'
+              onChange={(e) =>
+                setNewContact({
+                  ...newContact,
+                  [e.target.name]: e.target.value,
+                })
+              }
+            ></input>
+
+            <label>Email</label>
+            <input
+              type='email'
+              name='email'
+              onChange={(e) =>
+                setNewContact({
+                  ...newContact,
+                  [e.target.name]: e.target.value,
+                })
+              }
+            ></input>
+          </fieldset>
+          <div className='button_container'>
+            <button className="btn btn-primary p-2 m-2" style={{width:'fit-content'}} type='submit'>submit</button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default CreateContact;
